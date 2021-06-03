c54b5e1141412b6ebacbe8f8b45e223fabcb9186
AthulSudhakaran <>
2021-06-03 19:13:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+  var readlineSync=require("readline-sync");
+  var userName=readlineSync.question("what is ur name ");
+  console.log("welcome "+userName +" to DO YOU KNOW Athul");
+  var score=0;
+  function play(question,answer){
+  var userAnswer=readlineSync.question(question);
+    
+  if(userAnswer===answer){
+    console.log("u r right");
+    score=score+1;
+    
+  }
+  else{
+    console.log("u r wrong");
+
+  }
+  console.log("ur score is ", score);
+  }
+
+
+
+
+  var questions=[{
+    question:"where are you frm ",
+    answer:"kannur"
+  },
+  {
+    question:"my fav superhero would be ",
+    answer:"superman"
+  }
+  ]
+  for(var i=0;i<questions.length;i++){
+    var current=questions[i];
+    play(current.question,current.answer);
+  }
